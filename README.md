6f108390f7ec6692b71025502570ef26fd1d775b
Ibrahima SOUMARE <isoumare-ext@galec.fr>
2025-01-03 23:43:52 +0100

    add the temp dev

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 python3 --version
 
 `python3 -m venv venv_name
-`source venv/bin/activate
+`source venv_name/bin/activate
 `pip install -r requirements.txt 
--- main.py
+++ main.py
@@ -34,4 +34,27 @@ def download_file(bucket_name, nom_de_lobjet_telecharger, nom_en_local):
     blob.download_to_filename(nom_en_local)
     print(f"File {nom_de_lobjet_telecharger} downloaded to {nom_en_local}.")
 
+
+def copier_file_to(blob, folder):
+    ### Compléter le code
+
+def supprimer_object(blob):
+    ### Compléter le code
+
+def process_file(blob):
+    data = blob.download_as_text()
+    try:
+        df = pd.read_csv(io.StringIO(data))
+        ## Tester la conformité du fichier
+        ### Si tout est clean copier_file_to(blob, folder="clean") puis supprimer_object(blob)
+        ### S'il ya des erreurs copier_file_to(blob, folder="error") puis supprimer_object(blob)
+    except:
+        ### traiter les erreurs et si ca vient du fichier copier_file_to(blob, folder="error")
+
+def process_files(bucket_name):
+    blobs = lister_les_objects(bucket_name, prefix=None, delimiter=None)
+    for blob in blobs:
+        process_file(blob)
+
+
 download_file(BUCKET_NAME, "fichier_test.csv", "fichier_test.csv")
